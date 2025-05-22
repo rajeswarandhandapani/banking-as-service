f303c25d474fe53f784c4b7f4151f387f7931bc7
Rajeswaran Dhandapani <rajeswaran.dhandapani@gmail.com>
2025-05-22 17:41:31 -0400

    Implement account creation logic in SagaEventListener for ACCOUNT_CREATION events

--- account-service/src/main/java/com/rajeswaran/account/service/SagaEventListener.java
+++ account-service/src/main/java/com/rajeswaran/account/service/SagaEventListener.java
@@ -1,17 +1,41 @@
 package com.rajeswaran.account.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rajeswaran.account.entity.Account;
+import com.rajeswaran.common.dto.AccountDTO;
 import com.rajeswaran.common.dto.SagaEventDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import java.util.function.Consumer;
 
 @Service
 public class SagaEventListener {
+    @Autowired
+    private AccountService accountService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Bean
     public Consumer<SagaEventDTO> sagaEvents() {
         return event -> {
             System.out.println("Received saga event: " + event);
-            // TODO: Implement account creation logic based on eventType and payload
+            if ("START".equals(event.getEventType()) && "ACCOUNT_CREATION".equals(event.getSagaType())) {
+                try {
+                    AccountDTO accountDTO = objectMapper.readValue(event.getPayload(), AccountDTO.class);
+                    Account account = new Account(
+                        accountDTO.getAccountId(),
+                        accountDTO.getAccountNumber(),
+                        accountDTO.getAccountType(),
+                        accountDTO.getUserId(),
+                        accountDTO.getBalance(),
+                        accountDTO.getStatus()
+                    );
+                    accountService.createAccount(account);
+                    System.out.println("Account created as part of saga: " + account);
+                } catch (Exception e) {
+                    System.err.println("Failed to process saga event for account creation: " + e.getMessage());
+                }
+            }
         };
     }
 }
